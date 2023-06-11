2aa96ccc58fe6899eabff63ef2afd34c69b62235
Ruslan Kryskiv <kryskiv23@gmail.com>
2023-06-11 19:25:36 +0300

    Add comment to the application.

--- src/app_version2/MainRunner.java
+++ src/app_version2/MainRunner.java
@@ -2,6 +2,10 @@ package app_version2;
 
 public class MainRunner {
     public static void main(String[] args) throws InterruptedException {
+        /*
+             This is the final console version of the app "Converter_states"
+         */
+
         new UserGreeting().greeting();
     }
 }
